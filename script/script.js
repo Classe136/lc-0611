682c03a96cccca844c2bffc0a8d3148481d1dfbe
klelia <clelia.fradella@gmail.com>
2024-11-06 13:14:59 +0100

    esercizi
    Co-authored-by: Raffaele Pellegrino <RaffaelePellegrino@users.noreply.github.com>
    Co-authored-by: aj-herrera <ajherrera999@gmail.com>
    Co-authored-by: Francesco <FrancescoCialfiDev@users.noreply.github.com>
    Co-authored-by: Danilo Mosca <Danilo-Mosca@users.noreply.github.com>
    Co-authored-by: Andy Simota <AndySMT@users.noreply.github.com>
    Co-authored-by: Simone Fratini <Simone-Fratini@users.noreply.github.com>
    Co-authored-by: Orsouene <Orsouene@users.noreply.github.com>
    Co-authored-by: fabiolafisca <fabiolafisca@users.noreply.github.com>
    Co-authored-by: Andre050101 <Andre050101@users.noreply.github.com>
    Co-authored-by: EnricoGenovese <EnricoGenovese@users.noreply.github.com>
    Co-authored-by: Fabio Doria <DoriaFabio@users.noreply.github.com>
    Co-authored-by: DanielFerraiuolo98 <DanielFerraiuolo98@users.noreply.github.com>
    Co-authored-by: eleonoracappellesso <eleonoracappellesso@users.noreply.github.com>
    Co-authored-by: Arber Beshaj <Abraxas-7@users.noreply.github.com>
    Co-authored-by: CarloDettori <CarloDettori@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -18,7 +18,95 @@ THIS
 */
 
 const students = ["Michele", "Fabio", "Eleonora"];
+const numbers = [1, 99, 80, 100];
 
-function printHello(name) {
-  console.log("Hello " + name);
+function numSquare(num) {
+  console.log(num * num);
 }
+
+// scriviamo una funzione che stampi gli tutti gli elementi di un array
+
+function printArray(myarray, fn) {
+  console.log(fn);
+  for (let i = 0; i < myarray.length; i++) {
+    fn(myarray[i]);
+  }
+}
+
+// printArray(students, (name) => console.log("hello" + name));
+
+// printArray(numbers, numSquare);
+
+// const persona = {
+//   name: "Michele",
+//   surname: "Saraceni",
+//   camminare: function () {
+//     console.log("Michele sta camminando");
+//   },
+// };
+
+// persona.camminare();
+
+const isEven = (num) => num % 2 === 0;
+const printMyArray = (myarray) => {
+  for (let i = 0; i < myarray.length; i++) {
+    console.log("Hello" + " " + myarray[i]);
+  }
+};
+
+// printMyArray(students);
+
+// const pari = isEven(15);
+// console.log(pari);
+
+// metodo filter degli array restituisce un nuovo array contenente solo i valori dell'array iniziale che superano un determinato test o condizione
+
+const evenNumbers = numbers.filter(isEven);
+const studentLength5 = students.filter((el) => el.length > 5);
+
+// console.log(evenNumbers);
+// console.log(studentLength5);
+
+function myFilter(myArray, fnTest) {
+  //Crea nuovo array vuoto
+  const newArray = [];
+  console.log(fnTest);
+
+  //cicla sull'array che gli ho passato
+  for (let i = 0; i < myArray.length; i++) {
+    //per ogni elemnto dell'array esegue la funzione test che gli ho passato se la funzione ritorana true l'elemnto viene messo nell nuovo array altrimenti viene scartato
+    if (fnTest(myArray[i])) {
+      newArray.push(myArray[i]);
+    }
+  }
+  return newArray;
+}
+
+const testParola = (parola) => parola.length > 5;
+
+// let arrayFiltrato = myFilter(students, testParola);
+// console.log(arrayFiltrato);
+
+// arrayFiltrato = myFilter(numbers, isEven);
+// console.log(arrayFiltrato);
+
+// scrivere una funzione che acceti un array di colori e ritorni un nuovo array con i colori che contengono la lettera r
+
+const colors = ["red", "blue", "yellow", "green"];
+
+function colorFilter(array) {
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].includes("r")) {
+      newArray.push(array[i]);
+    }
+
+    //console.log(array[i].includes("r"));
+  }
+
+  return newArray;
+}
+let risultato = colorFilter(colors);
+
+console.log(risultato);
