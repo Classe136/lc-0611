2200933dd39dc8540a88240c2109e584de8b21b1
klelia <clelia.fradella@gmail.com>
2024-11-06 09:25:58 +0100

    comments and todo

--- script/script.js
+++ script/script.js
@@ -1,2 +1,24 @@
 "use strict";
 console.clear();
+/*
+PASSAGGIO DI FUNZIONI COME PARAMETRI AD UN'ALTRA FUNZIONE (CALLBACK)
+
+FUNZIONI ANONIME
+
+ASSEGNAZIONE DI FUNZIONI AD UNA VARIABILE
+
+ARROW FUNCTION
+
+*************************************************
+
+OGGETTI PROPRIETA' E METODI
+
+THIS
+
+*/
+
+const students = ["Michele", "Fabio", "Eleonora"];
+
+function printHello(name) {
+  console.log("Hello " + name);
+}
